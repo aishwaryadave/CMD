3b299bd3f359b8b74fa0eb020763f3889b05cf44
aishwaryadave <63992831+aishwaryadave@users.noreply.github.com>
2022-01-07 17:21:23 +0530

    commit2

--- src/main/java/com/cms/controllers/ContactsController.java
+++ src/main/java/com/cms/controllers/ContactsController.java
@@ -10,46 +10,52 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.cms.entities.Contact;
+
 import com.cms.services.ContactsService;
+
 @RestController
 public class ContactsController {
-	
-	
 	//autowire the BooksService class  
 	@Autowired  
 	ContactsService contactsService;  
+	
 	//creating a get mapping that retrieves all the books detail from the database   
 	@GetMapping("/contacts")  
 	private List<Contact> getAllContacts()   
 	{  
-	return contactsService.getAllContacts();  
+		return contactsService.getAllContacts();  
 	}  
+	
 	//creating a get mapping that retrieves the detail of a specific book  
 	@GetMapping("/contacts/{contactid}")  
 	private Contact getContacts(@PathVariable("contactid") int contactid)   
 	{  
-	return contactsService.getContactsById(contactid);  
+		return contactsService.getContactsById(contactid);  
 	}  
+	
 	//creating a delete mapping that deletes a specified book  
 	@DeleteMapping("/contacts/{contactid}")  
 	private void deleteBook(@PathVariable("contactid") int contactid)   
 	{  
-	contactsService.delete(contactid);  
+		contactsService.delete(contactid);  
 	}  
+	
 	//creating post mapping that post the book detail in the database  
 	@PostMapping("/contacts")  
 	private int saveBook(@RequestBody Contact contact)   
 	{  
-	contactsService.saveOrUpdate(contact);  
-	return contact.getcId();  
+		contactsService.saveOrUpdate(contact);  
+		return contact.getcId();  
 	}  
+	
 	//creating put mapping that updates the book detail   
 	@PutMapping("/contacts")  
 	private Contact update(@RequestBody Contact contact)   
 	{  
-	contactsService.saveOrUpdate(contact);  
-	return contact;  
+		contactsService.saveOrUpdate(contact);  
+		return contact;  
 	}  
 	}  
 
