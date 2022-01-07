57aced8a35a51e8db91cf10593873c63f2f2062c
aishwaryadave <63992831+aishwaryadave@users.noreply.github.com>
2022-01-07 17:34:44 +0530

    Commit 7

--- src/main/java/com/cms/services/ContactsService.java
+++ src/main/java/com/cms/services/ContactsService.java
@@ -13,36 +13,42 @@ import com.cms.entities.Contact;
 public class ContactsService {
 	@Autowired  
 	ContactsRepository contactsRepository;  
+	
 	//getting all books record by using the method findaAll() of CrudRepository  
 	public List<Contact> getAllContacts()   
 	{  
-	List<Contact> contacts = new ArrayList<Contact>();  
-	contactsRepository.findAll().forEach(contacts1 -> contacts.add(contacts1));  
-	return contacts;  
+		List<Contact> contacts = new ArrayList<Contact>();  
+		contactsRepository.findAll().forEach(contacts1 -> contacts.add(contacts1));  
+		return contacts;  
 	}  
+	
 	//getting a specific record by using the method findById() of CrudRepository  
 	public Contact getContactsById(int id)   
 	{  
-	return contactsRepository.findById(id).get();  
+		return contactsRepository.findById(id).get();  
 	}  
+	
 	//saving a specific record by using the method save() of CrudRepository  
 	public void saveOrUpdate(Contact contact)   
 	{  
-     contactsRepository.save(contact);  
+     		contactsRepository.save(contact);  
 	}  
+	
 	//deleting a specific record by using the method deleteById() of CrudRepository  
 	public void delete(int id)   
 	{  
-     contactsRepository.deleteById(id);  
+     		contactsRepository.deleteById(id);  
 	}  
+	
 	//updating a record  
 	public void update(Contact contact, int contactid)   
 	{  
-	contactsRepository.save(contact);  
+		contactsRepository.save(contact);  
 	} 
+	
 	public Contact save(Contact contact)   
 	{  
-     return contactsRepository.save(contact);  
+     		return contactsRepository.save(contact);  
 	}  
 
 }
