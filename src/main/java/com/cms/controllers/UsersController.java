7ae74603f04bb30264d32966efe595f45a7c8968
aishwaryadave <63992831+aishwaryadave@users.noreply.github.com>
2022-01-07 17:30:28 +0530

    commit 4

--- src/main/java/com/cms/controllers/UsersController.java
+++ src/main/java/com/cms/controllers/UsersController.java
@@ -1,7 +1,5 @@
 package com.cms.controllers;
 
-
-
 import java.security.Principal;
 
 import java.util.Optional;
@@ -13,8 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,13 +23,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 import com.cms.dao.ContactsRepository;
 import com.cms.dao.UserRepository;
+
 import com.cms.entities.Contact;
 import com.cms.entities.User;
-
-
 
 @Controller
 @RequestMapping("/user")
@@ -41,16 +40,15 @@ public class UsersController {
 	public void addCommonData(Model model, Principal principal) {
 		String userName= principal.getName();
 		User user = userRepository.findByUsername(userName).get();
-		System.out.println("user "+user);
-		System.out.println("USERNAME IS "+ userName);
 		model.addAttribute("user", user);
 	}
+	
 	@RequestMapping("/index")
 	public String dashboard(Model model, Principal principal) {
-		
 		model.addAttribute("title", "User dashboard");
 		return "normal/user_dashboard";
 	}
+	
 	@GetMapping("/add-contact")
 	public String openAddContactForm(Model model) {
 		model.addAttribute("title", "Add Contact");
@@ -61,45 +59,33 @@ public class UsersController {
 	@GetMapping("/settings")
 	public String settings(Model model) {
 		model.addAttribute("title", "Settings");
-		
 		return "normal/settings";
 	}
 	
 	@GetMapping("/profile")
 	public String profile(Model model) {
 		model.addAttribute("title", "My Profile");
-		
 		return "normal/profile";
 	}
+	
 	@PostMapping("/process-contact")
 	public String processContact(@ModelAttribute Contact contact, Principal principal, HttpSession session)
 	{
-			try{String name= principal.getName();
-		User user = this.userRepository.findByUsername(name).get();
-		
-		
-		contact.setUser(user);
-		user.getContacts().add(contact);
-		this.userRepository.save(user);
-		System.out.println("DATA :"+contact);
-		System.out.println("Added to database.");
+		try{
+			String name= principal.getName();
+		        User user = this.userRepository.findByUsername(name).get();
+		        contact.setUser(user);
+		        user.getContacts().add(contact);
+		        this.userRepository.save(user);
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
-		
-		
-		
-	
 		return "redirect:/user/show-contacts/0";	
 	}
+	
 	@GetMapping("/show-contacts/{page}")
 	public String showContacts(@PathVariable("page") Integer page,Model m, Principal principal) {
 		m.addAttribute("title", "Show User Contacts");
-		/*
-		 * String userName= principal.getName(); User user=
-		 * this.userRepository.getUserByUserName(userName); List<Contact> contacts =
-		 * user.getContacts();
-		 */
 		String userName = principal.getName();
 		User user = this.userRepository.findByUsername(userName).get();
 		Pageable pageable= PageRequest.of(page, 5);
@@ -109,9 +95,9 @@ public class UsersController {
 		m.addAttribute("totalPages", contacts.getTotalPages());
 		return "normal/show_contacts";
 	}
+	
 	@RequestMapping("/{cId}/contact")
 	public String showContactDetail(@PathVariable("cId") Integer cId, Model model, Principal principal) {
-		System.out.println("cId "+cId);
 		Optional<Contact> contactOptional = this.contactRepository.findById(cId);
 		Contact contact = contactOptional.get();
 		String userName= principal.getName();
@@ -120,19 +106,18 @@ public class UsersController {
 			model.addAttribute("contact", contact);
 			model.addAttribute("title",contact.getName());
 		}
-		
-		
 		return "normal/contact_detail";
 	}
+	
 	@GetMapping("/delete/{cid}")
 	public String deleteContact(@PathVariable("cid") Integer cId, Model model, HttpSession session, Principal principal) {
 		Contact contact = this.contactRepository.findById(cId).get();
-		
 		User user= this.userRepository.findByUsername(principal.getName()).get();
 		user.getContacts().remove(contact);
 		this.userRepository.save(user);
 		return "redirect:/user/show-contacts/0";
 	}
+	
 	@PostMapping("/update-contact/{cid}")
 	public String updateForm(@PathVariable("cid") Integer cid,Model m) {
 		m.addAttribute("title", "Update Form");
@@ -140,30 +125,20 @@ public class UsersController {
 		m.addAttribute("contact", contact);
 		return "normal/update_form";
 	}
+	
 	@RequestMapping(value="/process-update",method=RequestMethod.POST)
 	public String updateHandler(@ModelAttribute Contact contact, Model m, HttpSession session, Principal principal) {
 		try {
 			Contact oldcontactDetail = this.contactRepository.findById(contact.getcId()).get();
-			
 			User user= this.userRepository.findByUsername(principal.getName()).get();
 			contact.setUser(user);
-			
 			this.contactRepository.save(contact);
 			
-			
 		} catch (Exception e) {
-			// TODO: handle exception
 			e.printStackTrace();
 		}
-		System.out.println("CONTACT NAME: "+contact.getName());
-		System.out.println("CONTACT ID: "+contact.getcId());
 		return "redirect:/user/"+contact.getcId()+"/contact";
 	}
 	
 
 }
-
-
-
-
-
